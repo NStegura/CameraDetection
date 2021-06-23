cea6a916b4963a71cc2cb3ce018f1d3499c2402c
nstegura <nstegura0@gmail.com>
2021-06-23 20:41:05 +0300

    add post func's in Video app

--- video/api.py
+++ video/api.py
@@ -1,4 +1,23 @@
-from fastapi import APIRouter
+import os
+import shutil
+from typing import List
+from fastapi import APIRouter, UploadFile, File
+from settings.settings import MEDIA_ROOT
 
-video_router = APIRouter()
 
+video_router = APIRouter(tags=['video'])
+
+
+@video_router.post('/')
+async def root(video: UploadFile = File(...)):
+    with open(os.path.join(MEDIA_ROOT, video.filename), 'wb') as buffer:
+        shutil.copyfileobj(video.file, buffer)
+    return {"file_name": video.filename}
+
+
+@video_router.post('/img')
+async def upload_image(files: List[UploadFile] = File(...)):
+    for img in files:
+        with open(os.path.join(MEDIA_ROOT, img.filename), 'wb') as buffer:
+            shutil.copyfileobj(img.file, buffer)
+    return {"file_name": files}
